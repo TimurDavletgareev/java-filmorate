581fe620f2a064db8e6be8826bb7b71e22eea6f4
TimurDV88 <kumba-yo@yandex.ru>
2023-04-19 21:56:38 +0800

    Sprint 9 third commit

--- src/main/java/ru/yandex/practicum/filmorate/controller/FrValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FrValidationException.java
@@ -1,20 +1,22 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+    По условиям теории мы используем спринг версии 2.7, а в статье о @ControllerAdvice говорится, что нужен Spring 3.2
+    Чтобы избежать вывода всего стэктрейса в консоль я добавил аннотацию
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR) перед исключением (благодаря той же статье, как всегда
+    спасибо за полезную информацию:)
+
+    Теперь при неправильном запросе получаем в консоли только сообщение из getMessage и три строки про нашу ошибку
+
+ */
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class FrValidationException extends RuntimeException {
 
     public FrValidationException(String message) {
         super(message);
     }
-
-    /*
-        Не разобрался, как с помощью написанного исключения выводить в консоль краткое описание ошибки,
-        а не весь стэк-трэйс. Хотелось бы, чтобы в консоль выводилось сообщение как при использовании
-        spring-boot-starter-validation
-
-        И хотелось бы знать, как без использования исключений обрабатывать неподходящие запросы в контроллерах
-        и возвращать клиенту правильный статус-код и тело ответа. Подозреваю, что через аннотацию @ResponseStatus,
-        но дальше не разобрался.
-     */
-
 
 }
